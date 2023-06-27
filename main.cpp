b1867c7f7f32933defb9b177597a3b9ffd8a3f64
Jakub Dolecki <kubus.dolecki@gmail.com>
2023-06-27 02:24:01 +0200

    Implemented 'fortune | cowsay' on the index page

--- main.cpp
+++ main.cpp
@@ -7,10 +7,86 @@ namespace fs = std::filesystem;
 const static fs::path staticResourcesFolder = "static";
 
 #include <http.hpp>
+#include <html-builder.hpp>
 
 using namespace std::string_literals;
 
-bool requestListener(http::request &req) {
+std::string exec(std::string cmd) {
+	std::array<char, 128> buffer;
+	std::string result;
+	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+	if (!pipe) {
+		throw std::runtime_error("popen() failed!");
+	}
+	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+		result += buffer.data();
+	}
+	return result;
+}
+
+bool sendHTML(http::response &res, html::element &el) {
+	std::string content;
+	el.dumpToString(content);
+
+	res.setStatus(200);
+	res.setContentType(http::content_type::TEXT_HTML);
+	res.setContentString(content);
+	return res.send();
+}
+
+std::string escapeHtmlString(const std::string &code) {
+	std::string escaped;
+
+	for (const char c : code) {
+		switch (c) {
+			case '\n': {
+				escaped += "<br/>";
+			} break;
+			case ' ': {
+				escaped += "&nbsp;";
+			} break;
+			default: {
+				escaped += c;
+			}
+		}
+	}
+
+	return escaped;
+}
+
+html::div fortune() {
+	auto div = html::div();
+
+	div.addAttribute("id", "fortune");
+
+	std::string cowfile = exec("cowsay -l | awk 'NR>1 {print $1}' | shuf -n 1");
+	std::string cmd = "fortune | cowsay -f "s + cowfile;
+
+	div << (html::span().addAttribute("class", "terminal") << cmd) << html::br();
+
+	std::string cowspeach = exec("fortune | cowsay -f "s + cowfile);
+
+	div << (html::div() << escapeHtmlString(cowspeach));
+
+	return div;
+}
+
+bool serve_GET(http::request &req) {
+	if (req.url.path.size() == 0) { // index
+		auto html = html::html();
+
+		auto head = html::head();
+		head << html::link().addAttribute("rel", "stylesheet").addAttribute("href", "index.css");
+		html << head;
+
+		auto body = html::body();
+		body << (html::main() /* <<  */);
+		body << fortune();
+		html << body;
+
+		return sendHTML(req.response(), html);
+	}
+
 	size_t i = 0;
 	const auto &path = req.url.path;
 
@@ -21,6 +97,26 @@ bool requestListener(http::request &req) {
 	}
 
 	return req.response().sendFile(filepath);
+}
+
+bool requestListener(http::request &req) {
+	switch (req.method) {
+		case http::method::GET: {
+			return serve_GET(req);
+		} break;
+		case http::method::UNKNOWN:
+		case http::method::HEAD:
+		case http::method::POST:
+		case http::method::PUT:
+		case http::method::DELETE:
+		case http::method::CONNECT:
+		case http::method::OPTIONS:
+		case http::method::TRACE:
+		case http::method::PATCH:
+		default: {
+			throw http::exception(405, "Method unsupported");
+		}
+	}
 }
 
 bool dispatchError(http::request &req, int code, const std::string error) {
